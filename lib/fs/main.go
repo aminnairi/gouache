1b541346ac0c6d2ceca2b914a528141d49c64eb8
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-11-12 19:06:46 +0100

    initial commit

---
+++ lib/fs/main.go
@@ -0,0 +1,44 @@
+// Package fs gathers all of the function that helps manipulate the filesystem easily
+package fs
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/aminnairi/gouache/lib/logger"
+)
+
+func Files(fileOrDirectoryPath string) func(yield func(filePath string) bool) {
+	return func(yield func(filePath string) bool) {
+		fileOrDirectoryStat, fileOrDirectoryStatError := os.Stat(fileOrDirectoryPath)
+
+		if fileOrDirectoryStatError != nil {
+			logger.Fatal("Error while reading path "+fileOrDirectoryPath+":", fileOrDirectoryStatError.Error())
+			return
+		}
+
+		if !fileOrDirectoryStat.IsDir() {
+			yield(fileOrDirectoryPath)
+			return
+		}
+
+		walkError := filepath.WalkDir(fileOrDirectoryPath, func(path string, entry fs.DirEntry, err error) error {
+			if err != nil {
+				logger.Fatal("Error accessing path "+path+":", err.Error())
+				return nil
+			}
+
+			if entry.Type().IsRegular() {
+				if !yield(path) {
+					return filepath.SkipAll
+				}
+			}
+			return nil
+		})
+
+		if walkError != nil && walkError != filepath.SkipAll {
+			logger.Fatal("Error while walking though the directory "+fileOrDirectoryPath+":", walkError.Error())
+		}
+	}
+}
---
+++ lib/logger/main.go
@@ -0,0 +1,18 @@
+// Package logger gathers all of the functions useful for logging errors and messages to the console
+package logger
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+func Fatal(messages ...any) {
+	errorStyle := lipgloss.NewStyle().Bold(true).Background(lipgloss.Color("#B00020")).Foreground(lipgloss.Color("#FFFFFF")).PaddingLeft(1).PaddingRight(1)
+	allMessages := []any{errorStyle.Render("ERROR")}
+	allMessages = append(allMessages, messages...)
+
+	fmt.Println(allMessages...)
+	os.Exit(1)
+}
